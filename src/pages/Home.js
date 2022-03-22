6803120d9943f11c7ee2723f9b8f28b7a7bf2d53
Mariia <mariiashabelnik@gmail.com>
2022-03-22 08:36:31 +0100

    add img_video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,14 +7,9 @@ function Home() {
       <Helmet>
         <title>Mariia webshop - Home</title>
       </Helmet>
+
       <div className="shopping">
         <img src="media/image.jpg" alt="shopping" />
-      </div>
-      <div className="videoDiv">
-        <video id="video" autoplay muted loop>
-          <source src="media/production_video.mp4" type="video/mp4" />
-          Your browser does not support HTML5 video.
-        </video>
       </div>
     </div>
   );
